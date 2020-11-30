c2b2f127dc841568c4b1e945467b6919f3b16b9d
Charles <charlessm28@gmail.com>
2020-11-30 02:45:09 -0500

    Respaldo maqueta pv

--- resources/assets/js/components/PuntoVenta.vue
+++ resources/assets/js/components/PuntoVenta.vue
@@ -1,8 +1,8 @@
 <template>
         <main class="main">
-            <div class="container">
+            <div>
                 <!-- Ejemplo de tabla Listado -->
-                <div class="card" v-if="position==1">
+                <div class="card" v-show="position==1">
                     <div class="card-header">
                         <div class="row">
                             
@@ -18,12 +18,19 @@
                         </div>
                         <div class="row">                            
                             <div class="col-12">
-                                <select class="form-control" multiple  placeholder="Categoria" size ="2">
-                                    <option value="">-Seleccionar Categoria(s)-</option>
-                                    <option value="1">Categoria 1</option>
-                                    <option value="2">Categoria 2</option>
+                                <div class="form-group">
+                                <select class="mul-select" multiple="true">
+                                    <option value="Cambodia">Cambodia</option>
+                                    <option value="Khmer">Khmer</option>
+                                    <option value="Thiland">Thiland</option>
+                                    <option value="Koren">Koren</option>
+                                    <option value="China">China</option>
+                                    <option value="English">English</option>
+                                    <option value="USA">USA</option>
                                 </select>
+                            </div> 
                             </div>
+                            
                         </div>
                     </div>
                     <div class="card-body">
@@ -121,41 +128,60 @@
                                 </div>
                                 
                             </div>
-                            <div class="row mt-1">
-                                <div class="col-12">
-                                    <a @click="position=2" class="btn btn-block btn-lg active btn-success" href="#" role="button">Facturar</a>
-                                </div>
-                            </div>
+                            
                         </div>
 
                     </div>
                 </div>
 
-                <div class="card" v-if="position==2">
+                <div class="card" v-show="position==2">
                     <div class="row mt-1">
-                                    <div class="col-12">
-                                        <div class="card">
-                                        <div class="card-header">
-                                            <div class="row">
-                                                    <div class="col-9">
-                                                        
-                                                    </div>
-                                                    <div class="col-3">
-                                                        
-                                                    </div>
-                                                
-                                            </div>
-                                            <div class="row">                            
-                                                <div class="col-12">
-                                                   
-                                                </div>
-                                            </div>
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <div class="row">
+                                        <div class="col-9">
+                                            <button class="btn btn-secondary" @click="position=1">Productos</button>
                                         </div>
+                                        <div class="col-3">
+                                            <button type="button" class="btn btn-outline-primary" style="margin-left: -69px;">
+                                                Descartar
+                                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor"       xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+                                                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+                                                </svg>
+                                            </button>
+                                        </div>
+                                        
+                                    </div>
+                                    <div class="row">                            
+                                        <div class="col-12">
+                                            
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="card-body">
                                     <div class="form-group">
                                         <div class="row">
+                                            <div class="input-group mb-3">
+                                                
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="inputGroup-sizing-default">Cliente</span>
+                                                </div>
+                                                <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                                            </div>
                                             
-                                            
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <label class="input-group-text" for="inputGroupSelect01">Mezas</label>
+                                                </div>
+                                                <select class="custom-select" id="inputGroupSelect01">
+                                                    <option selected>Ubicacion...</option>
+                                                    <option value="1">meza 1</option>
+                                                    <option value="2">meza 2</option>
+                                                    <option value="3">meza 3</option>
+                                                </select>
+                                            </div>
                                         </div>
                                         <div class="row mt-1">
                                             <div class="col-12">
@@ -167,10 +193,14 @@
                                 </div>
 
                             </div>
-                            VISTA DE FACTURA 
-                            <button class="btn btn-secondary" @click="position=1">Productos</button>
+                                VISTA DE FACTURA 
+                               
                         </div>
-
+                    </div>
+                </div>
+                <div class="row mt-1">
+                    <div class="col-12">
+                        <a @click="position=2" class="btn btn-block btn-lg active btn-success" href="#" role="button">Facturar</a>
                     </div>
                 </div>
             </div>
@@ -179,6 +209,7 @@
 </template>
 
 <script>
+
     import vSelect from 'vue-select';
     export default {
         props : ['ruta'],
@@ -1762,6 +1793,12 @@
             },
         },
         mounted() {
+             $(".mul-select").select2({
+                    placeholder: "selecione categoria", //placeholder
+                    tags: true,
+                    tokenSeparators: ['/',',',';'," "] 
+                });
+                console.log( $(".mul-select"));
             let me= this;
             var d = new Date();
             
@@ -1790,7 +1827,10 @@
         }
     }
 </script>
-<style>    
+<style> 
+    .mul-select  {
+         width: 100%;
+    }
     .precio-prod{
             position: relative;
             float: right;
