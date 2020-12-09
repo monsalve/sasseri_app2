36b05b151db0ed12072df17c4d311665113991a5
isalazar00 <issalazar00@gmail.com>
2020-12-09 17:26:52 -0500

    Verificar actualizacion

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -68,10 +68,15 @@
                                     <a href="#"><i class="fa fa-cutlery"></i> Mesas</a>
                                 </li>
                                 <li @click="menu=27" class="collapse-item">
-                                    <a href="#"><i class="fa fa-usd"></i></i> IVAs</a>
+                                    <a href="#"><i class="fa fa-percent"></i> IVAs</a>
                                 </li>
                                 <li @click="menu=29" class="collapse-item">
-                                    <a href="#"><i class="fa fa-square-o"></i></i> Cajas</a>
+                                    <a href="#">
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calculator" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M12 1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
+                                        <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5v-2zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-4z"/>
+                                    </svg>
+                                    Cajas</a>
                                 </li>
                             </div>
 
@@ -86,10 +91,10 @@
                         data-parent="#accordionSidebar">
                             <div class="collapse-inner">
                                 <li @click="menu=15" class="collapse-item">
-                                    <a href="#" ><i class="fa fa-table"></i></i> Articulo</a>
+                                    <a href="#" ><i class="fa fa-table"></i> Articulo</a>
                                 </li>
                                 <li @click="menu=17" class="collapse-item">
-                                    <a href="#" ><i class="fa fa-share"></i></i> Entradas</a>
+                                    <a href="#" ><i class="fa fa-share"></i> Entradas</a>
                                 </li>
                                 <li @click="menu=18" class="collapse-item">
                                     <a href="#" ><i class="fa fa-reply"></i> Salidas</a>
